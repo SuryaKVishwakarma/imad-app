9643bfc1ec23126605427ea9807117c1c1ac34b9
Surya Kumar Vishwakarma <30697977+SuryaKVishwakarma@users.noreply.github.com>
2017-08-17 19:42:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/artical-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 })
 app.get('/artical-two',function(req,res){
-    res.send("Article two requsted and will be serverd here")
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 })
 app.get('/artical-three',function(req,res){
-    res.send("Article three requsted and will be serverd here")
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 })
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
