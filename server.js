33dd319aa3283eff23ec8fd2f5589912c7ec1791
Surya Kumar Vishwakarma <30697977+SuryaKVishwakarma@users.noreply.github.com>
2017-08-18 18:33:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,18 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title:'aritcle-one|surya',
+    heading:'article-one',
+    date: 'sep 05 2016',
+    content: `  <p>
+                                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                                </p>
+                                <p>
+                                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                                </p>
+                                <p>
+                                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                                </p>
+    `                            
+    
+};
+function createTamplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+var htmlTemplate = `
+<html>
+    <head>
+            <title>
+                 &{title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+             <link href="/ui/style.css" rel="stylesheet" />       
+        </head>
+                <body>
+                <div class="container">
+                            <div>
+                                <a href="/">Home</a>
+                            </div>
+                            <hr/>
+                            <h3>
+                        &{heading}
+                            </h3>    
+                            <div>
+                                &{date}
+                            </div> 
+                            <div>               
+                                &{content}
+                            </div>
+                </div>
+            </body>
+</html>
+`;
+return htmlTamplate;
+}
+
+app.get('/article-one', function (req, res) {
+  res.send(createTamplate(articleone));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
-app.get('/artical-two',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-})
-app.get('/artical-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
